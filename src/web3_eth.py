6d1607186df994dcdac4c97e2046b839350653e1
Roy Shadmon <16313057+royshadmon@users.noreply.github.com>
2023-03-27 09:50:52 -0700

    some fixes and removing the default account specification

--- src/web3_eth.py
+++ src/web3_eth.py
@@ -5,28 +5,20 @@ import os
 import glob
 from subprocess import Popen
 from eth_account.signers.local import LocalAccount
+import solcx
 
 
 # Class object to interact with Ethereum Contracts
 class Web3_Eth:
-    def __init__(self, provider: str, account=None, solidity_version='0.8.17', private_key=None):
+    def __init__(self, provider: str, solidity_version='0.8.17', private_key=None, connection_timeout=600):
         self.provider = provider
         self.private_key = private_key
         self.solidity_version = solidity_version
+        self.connection_timeout = connection_timeout
         self.w3 = self.web3_connect(provider)
 
-
-        if account:
-            if self.w3.isAddress(account):
-                self.account = account # public key
-            else:
-                print("Specified Account isn't valid")
-                exit(-1)
-        else:
-            self.account = None
-
     def web3_connect(self, provider):
-        w3 = Web3(HTTPProvider(provider))
+        w3 = Web3(HTTPProvider(provider, request_kwargs={'timeout': self.connection_timeout}))
         try:
             assert(w3.is_connected())
         except AssertionError:
@@ -34,12 +26,9 @@ class Web3_Eth:
             exit(-1)
         return w3
 
-    def compile_contract(self, contract_path, output_directory, solidity_version="0.8.17"):
-        contract_name = contract_path.split('/')[-1].split('.')[0]
-        # Run the javascript compile contract command
-        compile_command = f"solcjs --bin --abi {contract_path} --output-dir {output_directory}"
-        import solcx
-        self.w3.eth.default_account = self.w3.eth.accounts[0]
+    def compile_contract(self, contract_path, output_directory, private_key, solidity_version="0.8.17"):
+
+
         with open(contract_path, 'r') as f:
             source = f.read()
 
@@ -48,27 +37,6 @@ class Web3_Eth:
         compiled = solcx.compile_source(source, output_values=['abi', 'bin'])
         id, interface = compiled.popitem()
         return id, interface
-
-
-        store_var_contract = self.w3.eth.contract(address=address, abi=interface["abi"])
-
-        transaction = store_var_contract.functions.register().build_transaction(
-            {
-                # 'gas': 10000000,  # 3172000
-                'chainId': 1337,  # Ganache chain id
-                # 'gasPrice': int(web3_interface.w3.eth.gas_price),
-                'nonce': self.w3.eth.get_transaction_count("0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"),
-                # 'value': 0,
-            }
-        )
-        signed_tx = self.w3.eth.account.sign_transaction(transaction, "0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d")
-
-        tx_hash = self.w3.eth.send_raw_transaction(signed_tx.rawTransaction)
-
-        tx_receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
-        # result = store_var_contract.functions.register().transact()
-
-        print(store_var_contract.events.EM_CREATED().process_receipt(tx_receipt))
 
 
 
@@ -98,11 +66,8 @@ class Web3_Eth:
         )
 
         signed_tx = self.signTransaction(transaction, account_private_key)
-
         tx_hash = self.sendRawTransaction(signed_tx)
-
         tx_receipt = self.waitForTransactionReceipt(tx_hash)
-
         contract_address = self.getContractAddressFromTxReceipt(tx_receipt)
 
         store_var_contract = self.w3.eth.contract(address=contract_address, abi=interface["abi"])
@@ -135,8 +100,6 @@ class Web3_Eth:
         return acct_address == sender_account
 
     def get_eth_user(self, private_key):
-        # return self.w3.eth.account.privateKeyToAccount(
-        #     private_key)
         return self.w3.eth.account.from_key(private_key)
 
     def getTransactionCount(self, user: LocalAccount):
